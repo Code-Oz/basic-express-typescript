d7f06ef39f718c87d307b877168361983e33830e
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-06-13 16:37:15 -0700

    port 8080 & dev2

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,10 @@ import express, { Application, Request, Response } from 'express'
 
 const app: Application = express()
 
-const port: number = 3001
+const port = process.env.PORT || 8080;
 
 app.get('/toto', (req: Request, res: Response) => {
-    res.send('Hello toto')
+    res.send('Hello toto from the far north')
 })
 
 app.listen(port, function () {
