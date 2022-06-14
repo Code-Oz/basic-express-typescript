a086f33ef7420fcde51eadf789bf0b76e09800e4
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-06-13 19:11:26 -0700

    root and * routes in app

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,16 @@ const app: Application = express()
 
 const port = process.env.PORT || 8080;
 
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello root')
+})
+
 app.get('/toto', (req: Request, res: Response) => {
     res.send('Hello toto from the far north')
+})
+
+app.get('*', (req: Request, res: Response) => {
+    res.status(404).send("File not found");
 })
 
 app.listen(port, function () {
